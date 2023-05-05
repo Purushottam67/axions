beae970871ef06c0c8bd532cf14c6ba103a4b4ee
Purushottam mishra <78618623+Purushottam67@users.noreply.github.com>
2023-05-05 07:14:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageChanger from './components/atoms/ImageChanger';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageChanger/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
